1be7c89def00ef6c09a8f10151170928b458011a
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2019-12-01 10:48:12 +0530

    API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
 
 import '../services/location.dart';
 
@@ -17,6 +19,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await uLoc.getLocation();
     print(uLoc.longitude+uLoc.latitude);
   }
+
+
+  void getData()async {
+    http.Response response=await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    print(response.body);
+  }
   
 
   @override
@@ -27,6 +35,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     super.initState();
 
     getLocation();
+    getData();
     //gets location of user with out any user interactiom
   }
 
