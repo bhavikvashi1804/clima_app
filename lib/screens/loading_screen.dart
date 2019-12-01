02aadae32c0cc853a38e73b4e8673e2e7f554a1d
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2019-12-01 11:46:30 +0530

    CP 002

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 import '../services/location.dart';
@@ -24,6 +25,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getData()async {
     http.Response response=await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
     print(response.body);
+    var data=response.body;
+
+    double temperature=jsonDecode(data)['main']['temp'];
+    print(temperature);
+
+    String cityName=jsonDecode(data)['name']; 
+    print(cityName);
+  
+    int id=jsonDecode(data)['weather'][0]['id'];
+    print(id);    
+
+    
   }
   
 
